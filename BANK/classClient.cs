41e1a4b1fbc3a3b02521cccc9e4701e90ead20b6
aliya1988 <aliyam1988@mail.ru>
2022-02-26 14:30:38 +0300

    classes to files

---
+++ BANK/classClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BANK
+{
+    class Client
+    {
+        private string firstName, secondName;
+        public Client()
+        {
+            firstName = "default value";
+            secondName = "default value";
+        }
+    }
+}
